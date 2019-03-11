d36c923ca9e148e5218a5ecfdd3ff9acce827090
b3m2a1 <b3m2a1@gmail.com>
2019-03-11 02:30:33 -0700

    Building is ~less fun~

--- build.py
+++ build.py
@@ -8,18 +8,30 @@ target_arch = Env.system_name()+("-x86-64" if Env.get_is_64_bit() else "")
 target_bin_path = os.path.join(download_root, target_arch)
 
 if not os.path.exists(target_bin_path):
-    from io import BytesIO
-    from zipfile import ZipFile
-    from urllib.request import urlopen
-    import ssl
 
     url_root = "https://www.wolframcloud.com/objects/b3m2a1/MathLinkBinaries"
     url_targ = url_root+"/"+target_arch+".zip"+"?_key="+math_link_key
 
-    context = ssl._create_unverified_context()
-    resp = urlopen(url_targ, context=context)
-    zipfile = ZipFile(BytesIO(resp.read()))
-    zipfile.extractall(download_root)
+    try:
+        import wget
+        from zipfile import ZipFile
+
+        tmp_ = os.path.join(lib_root, target_arch+".zip")
+        wget.download(url_targ, tmp_)
+        zipfile = ZipFile.open(tmp_)
+        zipfile.extractall(download_root)
+        os.remove(tmp_)
+
+    except ImportError:
+        from io import BytesIO
+        from zipfile import ZipFile
+        from urllib.request import urlopen
+        import ssl
+
+        context = ssl._create_unverified_context()
+        resp = urlopen(url_targ, context=context)
+        zipfile = ZipFile(BytesIO(resp.read()))
+        zipfile.extractall(download_root)
 
 from PJLink.PJLinkNativeLibrary.load_lib import _get_lib_dir_loc, _NATIVE_LIBRARY_SHARED_OBJECT
 
