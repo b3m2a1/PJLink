c6118f72f790f70a5fad7ed03687cec460aa3bae
b3m2a1 <b3m2a1@gmail.com>
2019-03-19 00:58:43 -0700

    Windows tests

--- PJLink/PJLinkNativeLibrary/lib.py
+++ PJLink/PJLinkNativeLibrary/lib.py
@@ -6,10 +6,12 @@ try:
     sys.path.insert(0, loc)
     if not libdata._NATIVE_LIBRARY_EXISTS:
         raise ImportError("No PJLinkNativeLibrary to load")
-    if platform.platform() == "Windows":
-        so = libdata._NATIVE_LIBRARY_SHARED_OBJECT
-        base, ext = os.path.splitext(so)
-        shutil.copy(so, base+"_d"+ext)
+    # if platform.platform() == "Windows":
+    #     so = libdata._NATIVE_LIBRARY_SHARED_OBJECT
+    #     base, ext = os.path.splitext(so)
+    #     shutil.copy(so, base+"_d"+ext)
+    for file in os.listdir(loc):
+        print(file)
     from PJLinkNativeLibrary import *
 finally:
     sys.path.pop(0)
